bfb841a244dce0b3818fa50a27eff4bf83384c31
sundeepkamath <sundeep.g.kamath@gmail.com>
2018-09-15 22:04:03 +0530

    Initial commit for LiskovSubstitutionPrinciple

---
+++ LiskovsSubstitutionPrinciple/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LiskovSubstitutionPrinciple
+{
+    class Program
+    {
+        static int Area(Rectangle rectangle) => rectangle.Width * rectangle.Height; 
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle(3, 4);
+            Console.WriteLine($"Area of rectangle ({rectangle}) is {Area(rectangle)}");
+
+            Rectangle square = new Square();
+            square.Width = 4;
+            Console.WriteLine($"Area of square ({square}) is {Area(square)}");
+        }
+    }
+}
---
+++ LiskovsSubstitutionPrinciple/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace LiskovSubstitutionPrinciple
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"Width: {Width}, Height: {Height}";
+        }
+    }
+}
